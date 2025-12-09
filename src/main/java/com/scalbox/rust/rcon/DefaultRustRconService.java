ac1a3173ee99a6278dab5c994bb8d25513008473
Scalbox <sysoftware.inc@gmail.com>
2025-12-09 13:35:52 +0100

    Add license information to multiple files based on Rust RCON project

--- src/main/java/com/scalbox/rust/rcon/DefaultRustRconService.java
+++ src/main/java/com/scalbox/rust/rcon/DefaultRustRconService.java
@@ -1,3 +1,17 @@
+/*
+ * This file is based on the open-source project "Rust RCON"
+ * (https://github.com/MrGraversen/rust-rcon), created by
+ * Martin Graversen and originally released under the MIT license.
+ *
+ * Original base package: io.graversen.rust.rcon
+ *
+ * The code in this fork
+ * (https://github.com/scalbox/rust-rcon) has been adapted and is
+ * maintained by scalbox and is distributed under the terms of the
+ * Apache License, Version 2.0. For more details, see the LICENSE
+ * file included in this repository.
+ */
+
 package com.scalbox.rust.rcon;
 
 import com.google.common.eventbus.EventBus;
